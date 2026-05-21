b3cfd5064246859859c6b7632f6ea86b19231d4d
FURKAN TURAL <furkanturalofficial@outlook.com>
2026-05-21 16:26:59 +0300

    Updated admin project to production-ready state.

--- Presentation/FurkanTural_Admin/Program.cs
+++ Presentation/FurkanTural_Admin/Program.cs
@@ -106,6 +106,7 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromHours(8);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
 });
 
 var app = builder.Build();
